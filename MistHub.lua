52e47042a021047c3d9e8d6e8621a212684900f7
MistHubRoblox <pedroman101090@gmail.com>
2026-09-18 01:22:13 -0300

    Update MistHub.lua

--- MistHub.lua
+++ MistHub.lua
@@ -1,5 +1,17 @@
 if not script_key then print("script_key was not found") return end
-if not script_id then print("script_id was not found") return end
+
+local script_ids = {
+    [16205713724] = "ac047e097e274932fb36f03b37fe0267",
+}
+
+if not script_id then
+    script_id = script_ids[game.GameId]
+end
+
+if not script_id then
+    print("script_id was not found")
+    return
+end
 
 repeat task.wait() until game:IsLoaded()
 
