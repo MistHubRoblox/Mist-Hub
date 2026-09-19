6eba2bd49a2d02df885721a92a6f6ebaf207c351
MistHubRoblox <pedroman101090@gmail.com>
2026-09-18 21:49:59 -0300

    Update MistHub.lua

--- MistHub.lua
+++ MistHub.lua
@@ -1,38 +1,50 @@
 local script_ids = {
-  [585595353122] = "ac047e097e274932fb36f03b37fe0267",
+    [585595353122] = "ac047e097e274932fb36f03b37fe0267",
 }
 
-local usingId = nil
+local usingId
+local gameId = game.GameId
 
-if not script_id then
-  usingId = script_ids[game.GameId]
+
+if script_id then
+    usingId = script_id
 else
-  usingId = script_id
+    usingId = script_ids[gameId]
 end
 
 if not usingId then
-  print("script_id was not found")
-  return
+    print("scriptid was not found")
+    return
 end
 
 repeat
-  task.wait()
+    task.wait()
 until game:IsLoaded()
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local ScriptContext = game:GetService("ScriptContext")
 
 local success, response = pcall(function()
-  pcall(function()
-    if getconnections then
-      for i, v in getconnections(ScriptContext.Error) do
-        v:Disable()
-      end
-    end
-  end)
-  loadstring(game:HttpGet(string.format("https://api.luarmor.net/files/v4/loaders/%s.lua", usingId)))()
+    pcall(function()
+        if getconnections then
+            for , v in getconnections(ScriptContext.Error) do
+                v:Disable()
+            end
+        end
+    end)
+
+    loadstring(
+        game:HttpGet(
+            string.format(
+                "https://api.luarmor.net/files/v4/loaders/%s.lua",
+                usingId
+            )
+        )
+    )()
 end)
 
 if not success then
-  return LocalPlayer:Kick("[Mist Hub] Failed to load script. Error message: " .. response)
-end
+    return LocalPlayer:Kick(
+        "[Mist Hub] Failed to load script. Error message: " .. tostring(response)
+    )
+end 
